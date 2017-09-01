2ac89e1ba43ec6bfdce9afa24ccda3b12d0d1db0
mjboothaus <mjboothaus@gmail.com>
2017-09-01 14:05:56 +1000

    First version of app that seems to work - still requires some validation / testing and error handling. Also need to look at CSS (Bootstrap) and making prettier.

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-import datetime
+from datetime import datetime, timedelta
 
 import pandas as pd
 import quandl
 from bokeh.charts import TimeSeries
 from bokeh.embed import components
 from bokeh.resources import INLINE
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
-class MyForm(FlaskForm):
+class StockChoiceForm(FlaskForm):
+    # TODO: Improve validation
     stock_ticker = StringField(u'Stock Ticker', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
@@ -47,52 +48,57 @@ Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    stock_ticker = None
-    form = MyForm()
+    stock_ticker = 'AAPL'  # Default initial value (Apple)
+    form = StockChoiceForm()
+    return render_template('app_choose_stock.html', form=form)
 
-    if not form.validate_on_submit():  # TODO: Need to remove not here - don't understand this
-        stock_ticker = form.stock_ticker.data
 
-        # Load list of stock codes (currently static CSV version downloaded from Quandl
+@app.route('/produce_plot', methods=['POST'])
+def produce_plot():
+    stock_ticker = request.form.get('stock_ticker')
 
-        stock_codes = pd.read_csv('WIKI-datasets-codes.csv', header=None)
-        stock_code_list = stock_codes[0].tolist()
-        stock_code_list = [w.replace('WIKI/', '') for w in stock_code_list]
+    script = None
+    div = None
+    nasdaq_url = ''
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
 
-        if stock_ticker not in stock_code_list:
-            stock_ticker = 'AAPL'
+    # Load list of stock codes (currently static CSV version downloaded from Quandl
 
-        quandl_reference = 'WIKI/' + stock_ticker
-        nasdaq_url = 'http://www.nasdaq.com/symbol/' + stock_ticker
+    stock_codes = pd.read_csv('WIKI-datasets-codes.csv', header=None)
+    stock_code_list = stock_codes[0].tolist()
+    stock_code_list = [w.replace('WIKI/', '') for w in stock_code_list]
 
-        # Determine period for last month
+    if stock_ticker not in stock_code_list:  # TODO - how do we do client-side validation of the stock code?
+        stock_ticker = 'AAPL'
 
-        end_date = datetime.date.today().replace(day=datetime.date.today().day - 1)
-        start_date = end_date.replace(month=end_date.month - 1)
+    quandl_reference = 'WIKI/' + stock_ticker
+    nasdaq_url = 'http://www.nasdaq.com/symbol/' + stock_ticker
 
-        # Get price data from Quandl
-        try:
-            quandl.ApiConfig.api_key = 'Mror8Ww3qg5e947A7Fhj'
-            data = quandl.get(quandl_reference, start_date=start_date, end_date=end_date, returns='pandas')
-        except:
-            print
-            print 'ERROR: Unable to query Quandl data - check internet connection?'
-            quit()
+    # Determine period for last month
 
-        js_resources = INLINE.render_js()
-        css_resources = INLINE.render_css()
+    end_date = datetime.today() - timedelta(days=1)
+    start_date = end_date - timedelta(days=31)  # just look at last 30 days for now (not exactly 1 month)
 
-        # Plotting
-        p = TimeSeries(data, y='Close', title=stock_ticker, ylabel='Price (USD / share)', xlabel='Date')
-        script, div = components(p)
+    # Get price data from Quandl
+    try:
+        quandl.ApiConfig.api_key = 'Mror8Ww3qg5e947A7Fhj'
+        data = quandl.get(quandl_reference, start_date=start_date, end_date=end_date, returns='pandas')
+    except:
+        print
+        print 'ERROR: Unable to query Quandl data - check internet connection?'
+        quit()  # TODO: Need to exit more gracefully
 
-    form.stock_ticker.data = stock_ticker
+    # Plotting
+
+    p = TimeSeries(data, y='Close', title=stock_ticker, ylabel='Price (USD / share)', xlabel='Date')
+    script, div = components(p)
 
     return render_template('app_plotter.html',
-                           form=form,
                            name=stock_ticker,
                            plot_script=script,
                            plot_div=div,
+                           nasdaq_url=nasdaq_url,
                            js_resources=js_resources,
                            css_resources=css_resources)
 
